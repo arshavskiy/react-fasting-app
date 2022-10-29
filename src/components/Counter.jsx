8a37056b1fd9f6ba7f17b18a51587f339a526787
pavel <arshavsky.pasha@gmail.com>
2022-10-29 22:52:04 +0300

    init react app

---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+    render() {
+        return (
+            <section className="app__counter">
+                Counter
+            </section>
+        );
+    }
+}
+
+export default Counter;
